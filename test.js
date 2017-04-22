152c19f60b7738f478d576f6acc6fb7448e79a9c
Sindre Sorhus <sindresorhus@gmail.com>
2017-04-22 20:02:16 +0700

    ES2015ify and require Node.js 4

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 import test from 'ava';
-import fn from './';
+import m from '.';
 
 test('main', t => {
-	t.is(fn('\nx\n'), 'x');
-	t.is(fn('\n\n\nx\n\n\n'), 'x');
-	t.is(fn('\r\nx\r\n'), 'x');
-	t.is(fn('\n\r\n\nx\n\r\n\n'), 'x');
+	t.is(m('\nx\n'), 'x');
+	t.is(m('\n\n\nx\n\n\n'), 'x');
+	t.is(m('\r\nx\r\n'), 'x');
+	t.is(m('\n\r\n\nx\n\r\n\n'), 'x');
 });
 
 test('start', t => {
-	t.is(fn.start('\nx'), 'x');
-	t.is(fn.start('\r\nx'), 'x');
-	t.is(fn.start('\n\n\n\nx'), 'x');
-	t.is(fn.start('\n\n\r\n\nx'), 'x');
-	t.is(fn.start('x\n\n\r\n\n'), 'x\n\n\r\n\n');
+	t.is(m.start('\nx'), 'x');
+	t.is(m.start('\r\nx'), 'x');
+	t.is(m.start('\n\n\n\nx'), 'x');
+	t.is(m.start('\n\n\r\n\nx'), 'x');
+	t.is(m.start('x\n\n\r\n\n'), 'x\n\n\r\n\n');
 });
 
 test('end', t => {
-	t.is(fn.end('x\n'), 'x');
-	t.is(fn.end('x\r\n'), 'x');
-	t.is(fn.end('x\n\n\n\n'), 'x');
-	t.is(fn.end('x\n\n\r\n\n'), 'x');
-	t.is(fn.end('\n\n\r\n\nx'), '\n\n\r\n\nx');
+	t.is(m.end('x\n'), 'x');
+	t.is(m.end('x\r\n'), 'x');
+	t.is(m.end('x\n\n\n\n'), 'x');
+	t.is(m.end('x\n\n\r\n\n'), 'x');
+	t.is(m.end('\n\n\r\n\nx'), '\n\n\r\n\nx');
 });
