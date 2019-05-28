0c2021a32fb6c589457628fedd38d42a7a674954
Michael Johnston <michael.johnston29@gmail.com>
2019-05-28 16:22:12 +1000

    removed unnecessary regex replace

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
-module.exports = string => string.replace(/^[\r\n]+/, '').replace(/[\r\n]+$/, '');
+module.exports = string => string.replace(/^[\r\n]+|[\r\n]+$/g, '');
 module.exports.start = string => string.replace(/^[\r\n]+/, '');
 module.exports.end = string => string.replace(/[\r\n]+$/, '');
