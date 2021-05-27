25246c6ce5eea1c82d448998733a6302a4350d91
Sindre Sorhus <sindresorhus@gmail.com>
2021-05-27 21:34:56 +0700

    Fix ReDoS vulnerability for the `.end()` method
    The other methods do not have this vulnerability, but I’m refactoring them while I’m at it.

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ import test from 'ava';
 import trimNewlines from './index.js';
 
 test('main', t => {
+	t.is(trimNewlines(''), '');
+	t.is(trimNewlines('  '), '  ');
+	t.is(trimNewlines('\n\n\r'), '');
 	t.is(trimNewlines('\nx\n'), 'x');
 	t.is(trimNewlines('\n\n\nx\n\n\n'), 'x');
 	t.is(trimNewlines('\r\nx\r\n'), 'x');
@@ -9,6 +12,9 @@ test('main', t => {
 });
 
 test('start', t => {
+	t.is(trimNewlines.start(''), '');
+	t.is(trimNewlines.start('  '), '  ');
+	t.is(trimNewlines.start('\n\n\r'), '');
 	t.is(trimNewlines.start('\nx'), 'x');
 	t.is(trimNewlines.start('\r\nx'), 'x');
 	t.is(trimNewlines.start('\n\n\n\nx'), 'x');
@@ -17,9 +23,42 @@ test('start', t => {
 });
 
 test('end', t => {
+	t.is(trimNewlines.end(''), '');
+	t.is(trimNewlines.end('  '), '  ');
+	t.is(trimNewlines.end('\n\n\r'), '');
 	t.is(trimNewlines.end('x\n'), 'x');
 	t.is(trimNewlines.end('x\r\n'), 'x');
 	t.is(trimNewlines.end('x\n\n\n\n'), 'x');
 	t.is(trimNewlines.end('x\n\n\r\n\n'), 'x');
 	t.is(trimNewlines.end('\n\n\r\n\nx'), '\n\n\r\n\nx');
+});
+
+test('main - does not have exponential performance', t => {
+	for (let index = 0; index < 45000; index += 1000) {
+		const string = String(Array.from({length: index}).fill('\n').join('')) + 'a' + String(Array.from({length: index}).fill('\n').join(''));
+		const start = Date.now();
+		trimNewlines(string);
+		const difference = Date.now() - start;
+		t.true(difference < 10, `Execution time: ${difference}`);
+	}
+});
+
+test('start - does not have exponential performance', t => {
+	for (let index = 0; index < 45000; index += 1000) {
+		const string = String(Array.from({length: index}).fill('\n').join('')) + 'a';
+		const start = Date.now();
+		trimNewlines.start(string);
+		const difference = Date.now() - start;
+		t.true(difference < 10, `Execution time: ${difference}`);
+	}
+});
+
+test('end - does not have exponential performance', t => {
+	for (let index = 0; index < 45000; index += 1000) {
+		const string = 'a' + String(Array.from({length: index}).fill('\n').join(''));
+		const start = Date.now();
+		trimNewlines.end(string);
+		const difference = Date.now() - start;
+		t.true(difference < 10, `Execution time: ${difference}`);
+	}
 });
