8e3ca5a85650dd24e68f5266f9b148115d137b94
Sindre Sorhus <sindresorhus@gmail.com>
2021-06-10 23:44:05 +0700

    Fix tests

--- test.js
+++ test.js
@@ -35,30 +35,42 @@ test('end', t => {
 
 test('main - does not have exponential performance', t => {
 	for (let index = 0; index < 45000; index += 1000) {
-		const string = String(Array.from({length: index}).fill('\n').join('')) + 'a' + String(Array.from({length: index}).fill('\n').join(''));
+		const string = 'a' + String(Array.from({length: index}).fill('\n').join('')) + String(Array.from({length: index}).fill('\n').join('')) + 'a';
 		const start = Date.now();
 		trimNewlines(string);
 		const difference = Date.now() - start;
 		t.true(difference < 10, `Execution time: ${difference}`);
+
+		if (difference > 10) {
+			throw new Error(); // eslint-disable-line unicorn/error-message
+		}
 	}
 });
 
 test('start - does not have exponential performance', t => {
 	for (let index = 0; index < 45000; index += 1000) {
-		const string = String(Array.from({length: index}).fill('\n').join('')) + 'a';
+		const string = 'a' + String(Array.from({length: index}).fill('\n').join(''));
 		const start = Date.now();
 		trimNewlines.start(string);
 		const difference = Date.now() - start;
 		t.true(difference < 10, `Execution time: ${difference}`);
+
+		if (difference > 10) {
+			throw new Error(); // eslint-disable-line unicorn/error-message
+		}
 	}
 });
 
 test('end - does not have exponential performance', t => {
 	for (let index = 0; index < 45000; index += 1000) {
-		const string = 'a' + String(Array.from({length: index}).fill('\n').join(''));
+		const string = String(Array.from({length: index}).fill('\n').join('')) + 'a';
 		const start = Date.now();
 		trimNewlines.end(string);
 		const difference = Date.now() - start;
 		t.true(difference < 10, `Execution time: ${difference}`);
+
+		if (difference > 10) {
+			throw new Error(); // eslint-disable-line unicorn/error-message
+		}
 	}
 });
